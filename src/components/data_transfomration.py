0d26be8d5ef598079b7745df8ac103bf3e7bdf84
Ryan Wilson <wilson.ryan.jay@gmail.com>
2023-11-06 13:03:03 -0600

    Update data_transfomration.py

--- src/components/data_transfomration.py
+++ src/components/data_transfomration.py
@@ -7,6 +7,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from sklearn.compose import make_column_selector as selector
 
 from src.exception import CustomException
 from src.logger import logging
@@ -22,50 +23,37 @@ class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
 
-    def get_data_transformer_object(self):
+    def get_data_transformer_object(self, train_data):
         '''
-        This function si responsible for data trnasformation
+        This function is responsible for data trnasformation
         
         '''
         try:
-            numerical_columns = ["writing_score", "reading_score"]
-            categorical_columns = [
-                "gender",
-                "race_ethnicity",
-                "parental_level_of_education",
-                "lunch",
-                "test_preparation_course",
-            ]
+            numeric_features = train_data.select_dtypes(exclude = 'object').columns
+            categorical_features = train_data.select_dtypes(include = 'object').columns
 
             num_pipeline= Pipeline(
                 steps=[
                 ("imputer",SimpleImputer(strategy="median")),
                 ("scaler",StandardScaler())
-
                 ]
             )
 
             cat_pipeline=Pipeline(
-
                 steps=[
-                ("imputer",SimpleImputer(strategy="most_frequent")),
-                ("one_hot_encoder",OneHotEncoder()),
-                ("scaler",StandardScaler(with_mean=False))
+                ("one_hot_encoder",OneHotEncoder(handle_unknown="ignore"))
                 ]
 
             )
 
-            logging.info(f"Categorical columns: {categorical_columns}")
-            logging.info(f"Numerical columns: {numerical_columns}")
+            logging.info(f"Categorical columns: {categorical_features}")
+            logging.info(f"Numerical columns: {numeric_features}")
 
             preprocessor=ColumnTransformer(
                 [
-                ("num_pipeline",num_pipeline,numerical_columns),
-                ("cat_pipelines",cat_pipeline,categorical_columns)
-
+                ("num_pipeline",num_pipeline, selector(dtype_exclude="object")),
+                ("cat_pipelines",cat_pipeline, selector(dtype_include="object"))
                 ]
-
-
             )
 
             return preprocessor
@@ -76,23 +64,25 @@ class DataTransformation:
     def initiate_data_transformation(self,train_path,test_path):
 
         try:
+            logging.info("Read train and test data started")
             train_df=pd.read_csv(train_path)
             test_df=pd.read_csv(test_path)
 
-            logging.info("Read train and test data completed")
-
-            logging.info("Obtaining preprocessing object")
-
-            preprocessing_obj=self.get_data_transformer_object()
-
-            target_column_name="math_score"
-            numerical_columns = ["writing_score", "reading_score"]
+            logging.info(f'Read train and test data completed {train_path}')
+            
+            logging.info("Extract input and target features started")
+            target_column_name="hs_diploma"
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
 
             input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
             target_feature_test_df=test_df[target_column_name]
+
+            logging.info("Extract input and target features completed")
+
+            logging.info("Obtaining preprocessing object")
+            preprocessing_obj=self.get_data_transformer_object(input_feature_train_df)
 
             logging.info(
                 f"Applying preprocessing object on training dataframe and testing dataframe."
